dcecb61a7bddf0a5b9c67e6a30895fbe8b26445d
duanyufei <duanyufei@bytedance.com>
2022-04-26 22:49:18 +0800

    format Google

--- main.c
+++ main.c
@@ -2,32 +2,30 @@
 #include <string.h>
 
 typedef struct {
-    unsigned int id;
-    unsigned int age;
-    char name[30];
+  unsigned int id;
+  unsigned int age;
+  char name[30];
 } Person;
 
-
-void printPersion(const Person* p) {
-    printf("id:%d\n", (*p).id);
-    printf("age:%d\n", (*p).age);
-    printf("name:%s\n", (*p).name);
+void printPersion(const Person *p) {
+  printf("id:%d\n", (*p).id);
+  printf("age:%d\n", (*p).age);
+  printf("name:%s\n", (*p).name);
 
 }
 int main(int argc, char *argv[]) {
 
-    char c;
-    while ((c=getchar())!=EOF)
-    {
-        printf("putchar\n");
-        putchar(c);
-    }
-    
-    // printf("argc: %d", argc);
-    // printf("\n");
+  char c;
+  while ((c = getchar()) != EOF) {
+    printf("putchar\n");
+    putchar(c);
+  }
 
-    // Person peter = { 123, 35, "Peter"};
+  // printf("argc: %d", argc);
+  // printf("\n");
 
-    // printPersion(&peter);
-    return 0;
+  // Person peter = { 123, 35, "Peter"};
+
+  // printPersion(&peter);
+  return 0;
 }
